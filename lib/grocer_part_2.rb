6fdc03ebb597553e87be46223a6e80e910d7eecf
Scott Gloyna <scott.gloyna01@gmail.com>
2021-01-07 03:52:16 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -19,30 +19,26 @@ def apply_coupons(cart, coupons)
     #binding.pry
     #if cart meets number requirement for coupon
     if line_item[:count] % saving[:num] == 0
-      #update line_item count = 0
-      #add new hash to cart with :item + "W/COUPON" and new per unit price
+
       coupon_item=line_item.clone
       coupon_item[:item] = coupon_item[:item] + " W/COUPON"
       coupon_item[:price] = saving[:cost]/saving[:num]
       line_item[:count]=0
       cart << coupon_item
-      #binding.pry
+
       elsif line_item[:count] > saving[:num]
-      #update line_item count = count - num
-      #add new hash to cart with :item + "W/COUPON" and new per unit price and num
-      #note: Need to add case when count is more than 2X num but not even multiple
+
       coupon_item=line_item.clone
       coupon_item[:item] = coupon_item[:item] + " W/COUPON"
       coupon_item[:price] = saving[:cost]/saving[:num]
-      #binding.pry
+
       coupon_item[:count]=line_item[:count]/saving[:num]*saving[:num]
-      #binding.pry
+
       line_item[:count]=line_item[count]-coupon_item[:cout]
       cart << coupon_item
-      #binding.pry
+
     end
   end
-  
 end
 
 def apply_clearance(cart)
