a6e7803a9002f83dd47f0fe4471eabef3f6634c0
Scott Gloyna <scott.gloyna01@gmail.com>
2021-01-07 04:09:04 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -48,7 +48,7 @@ def apply_clearance(cart)
   
   cart.each do |line_item|
     if line_item[:clearance]
-      line_item[:price] *= 0.2
+      line_item[:price] = (line_item[:price] * 0.2).round(2)
       binding.pry
     end
   end
