54b574aa9244bdebb5e1b2e0b328955c1ec3721a
Scott Gloyna <scott.gloyna01@gmail.com>
2021-01-07 03:32:15 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -18,9 +18,13 @@ def apply_coupons(cart, coupons)
     if line_item[:count] % saving[:num] == 0
       #update line_item count = 0
       #add new hash to cart with :item + "W/COUPON" and new per unit price
+      binding.pry
       coupon_item=line_item.clone
+      binding.pry
       coupon_item[:name] = coupon_item[:name] + "W/COUPON"
+      binding.pry
       line_item[:count]=0
+      binding.pry
       cart << coupon_item
       binding.pry
       
